9934f94a660f6f5132f96d8b5b26b3840754176d
Serhii Vorotnoi <svorotnoy@gmail.com>
2024-11-05 13:47:29 +0200

    step9: download

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import MiniSlider from "./modules/slider/slider-mini";
 import VideoPlayer from "./modules/playVideo";
 import Difference from "./modules/difference";
 import Form from "./modules/form";
+import ShowInfo from "./modules/showInfo";
+import Download from "./modules/download";
 
 window.addEventListener("DOMContentLoaded", () => {
   // Main Slider
@@ -72,4 +74,6 @@ window.addEventListener("DOMContentLoaded", () => {
   // Difference
   new Difference(".officerold", ".officernew", ".officer__card-item").init();
   new Form(".form").init();
+  new ShowInfo(".plus__content").init();
+  new Download(".download").init();
 });
